2801a8dece21f6fe40ef8658b15d6795df927793
akmarwah03 <amarwah4@my.bcit.ca>
2021-05-05 15:56:13 -0700

    Firebase and firereact config

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,26 @@
+import React from "react";
+import "firebase/firestore";
+import {
+  FirebaseAppProvider,
+  useFirestoreDocData,
+  useFirestore,
+} from "reactfire";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD7RM--kySDQtpuFkxa9IImfEl-4hlFo3k",
+  authDomain: "vocabwar.firebaseapp.com",
+  projectId: "vocabwar",
+  storageBucket: "vocabwar.appspot.com",
+  messagingSenderId: "919676092490",
+  appId: "1:919676092490:web:9a950472fcc297688626b3",
+};
 
 function App() {
-  return <h1>VocabWar</h1>;
+  return (
+    <FirebaseAppProvider firebaseConfig={firebaseConfig}>
+      <h1>VocabWar</h1>
+    </FirebaseAppProvider>
+  );
 }
 
 export default App;
