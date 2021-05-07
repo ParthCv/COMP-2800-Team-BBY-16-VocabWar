afcd65d1d01a9b4e133983c7cf16401f53f9ef7e
Parth Chaturvedi <pchaturvedi@my.bcit.ca>
2021-05-07 05:49:21 +0530

    Feature for timer

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,30 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import Timer from './game/Timer'
+import "firebase/firestore";
+import {
+  FirebaseAppProvider,
+  useFirestoreDocData,
+  useFirestore,
+} from "reactfire";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD7RM--kySDQtpuFkxa9IImfEl-4hlFo3k",
+  authDomain: "vocabwar.firebaseapp.com",
+  projectId: "vocabwar",
+  storageBucket: "vocabwar.appspot.com",
+  messagingSenderId: "919676092490",
+  appId: "1:919676092490:web:9a950472fcc297688626b3",
+};
 
 function App() {
-  return <h1>VocabWar</h1>;
+  return (
+   <>
+   <FirebaseAppProvider firebaseConfig={firebaseConfig}>   
+    <Timer minutes={0} seconds={10}></Timer>
+    </FirebaseAppProvider>
+  </>
+  );
 }
 
 export default App;
