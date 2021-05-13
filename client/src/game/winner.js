a456ab88847a3588a148481b6b31cb7fdd879aac
akmarwah03 <amarwah4@my.bcit.ca>
2021-05-12 17:02:29 -0700

    Winner screen styling

--- client/src/game/winner.js
+++ client/src/game/winner.js
@@ -1,46 +1,74 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import "./winner.css";
-import { useFirestoreDocData, useFirestore } from "reactfire";
+import { useFirestoreDocData } from "reactfire";
 
 // fetches the points from both players
 function WinnerPoints(props) {
-  const points1 = useFirestoreDocData(props.gameRef).data[
-    `p1Points`
-  ];
-  const points2 = useFirestoreDocData(props.gameRef).data[
-    `p2Points`
-  ];
+  const points1 = useFirestoreDocData(props.gameRef).data[`p1Points`];
+  const points2 = useFirestoreDocData(props.gameRef).data[`p2Points`];
+  const winner = useFirestoreDocData(props.gameRef).data?.winner;
   const id = `player${props.player}`;
-}
-  // make it so when the timer runs out, this function is called.
-  // return true if player 1 points > player 2 points and vice versa
-  function Winner() {
-    const [winner1, setWinner1] = useState(false);
-    const [winner2, setWinner2] = useState(false);
-    const chooseWinner = () => {
-      if (points1 > points2) {
-        setWinner1(true);
-      } else if (points2 > points1) {
-        setWinner2(true);
-      }
-    }
-  }
-  // need to make it so if player 1 = true, display you win and green line.
-  useEffect(() => {
 
-    });
+  useEffect(() => {
+    if (props.over) {
+      if (points1 > points2) {
+        props.gameRef.set(
+          {
+            winner: 1,
+          },
+          { merge: true }
+        );
+      } else if (points2 > points1) {
+        props.gameRef.set(
+          {
+            winner: 2,
+          },
+          { merge: true }
+        );
+      } else {
+        props.gameRef.set(
+          {
+            winner: 3,
+          },
+          { merge: true }
+        );
+      }
+      console.log(winner, props.player);
+    }
+  }, [props.over]);
+
   return (
-    <div className = 'overlay'>
-      <div id = "text">
-        <h1>You win</h1>
-      <div class = "win"></div>
-      <div id={id}>
-      <h2>Player {props.player}</h2>
-      <h2>{points1}</h2>
-    </div>
-    </div>
-    <button class = "button">Leave Lobby</button>
+    <div className='overlay_results'>
+      <div>
+        {winner != 3 ? (
+          winner == props.player ? (
+            <>
+              <h1 id='resultText'>You win</h1>
+              <div className='resultLine win'></div>
+            </>
+          ) : (
+            <>
+              <h1 id='resultText'>You lose</h1>
+              <div className='resultLine lose'></div>
+            </>
+          )
+        ) : (
+          <>
+            <h1 id='resultText'>Game Tied</h1>
+            <div className='resultLine tied'></div>
+          </>
+        )}
+        <div className='scoreResult'>
+          <h2 className='playerResult'>Player 1</h2>
+          <h2>{points1}pts</h2>
+        </div>
+        <div className='scoreResult'>
+          <h2 className='playerResult'>Player 2</h2>
+          <h2>{points2}pts</h2>
+        </div>
+      </div>
+      <button className='leaveLobbyResult'>Leave Lobby</button>
     </div>
   );
-
+}
 export default WinnerPoints;
