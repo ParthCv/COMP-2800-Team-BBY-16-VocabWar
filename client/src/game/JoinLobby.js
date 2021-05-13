0aa33905bb91ae1a2d370cebbd2b32a4a9aa8562
akmarwah03 <amarwah4@my.bcit.ca>
2021-05-13 09:07:04 -0700

    Fixed spelling mistake

--- client/src/game/JoinLobby.js
+++ client/src/game/JoinLobby.js
@@ -13,7 +13,7 @@ import "./joinLobby.css";
 
 const JoinLobby = () => {
   const [code, setCode] = useState("");
-  const [correctCode, SetCorrectCode] = useState("")
+  const [correctCode, SetCorrectCode] = useState("");
   const overlay = useRef(0);
   const gameRef = useFirestore().collection("Games");
 
@@ -33,11 +33,12 @@ const JoinLobby = () => {
         let p1 = await gameDoc.data().p1;
         let p2 = await gameDoc.data().p2;
         if (p2) {
-          document.getElementById('joinBtn').innerHTML = "Lobby Full";
-          document.getElementById('joinBtn').style.backgroundColor = "#E74C3C";
+          document.getElementById("joinBtn").innerHTML = "Lobby Full";
+          document.getElementById("joinBtn").style.backgroundColor = "#E74C3C";
           setTimeout(() => {
-            document.getElementById('joinBtn').innerHTML = "Join";
-            document.getElementById('joinBtn').style.backgroundColor = "#E67E22";
+            document.getElementById("joinBtn").innerHTML = "Join";
+            document.getElementById("joinBtn").style.backgroundColor =
+              "#E67E22";
           }, 1000);
           throw new Error("Lobby already full");
         }
@@ -47,21 +48,21 @@ const JoinLobby = () => {
               p2: "hey",
             },
             { merge: true }
-          );  
+          );
           console.log("correct code=>" + code);
         }
       } catch (err) {
         console.log("game not found", err);
-        document.getElementById('joinBtn').innerHTML = "Worng Code";
-        document.getElementById('joinBtn').style.backgroundColor = "#E74C3C";
+        document.getElementById("joinBtn").innerHTML = "Wrong Code";
+        document.getElementById("joinBtn").style.backgroundColor = "#E74C3C";
         setTimeout(() => {
-            document.getElementById('joinBtn').innerHTML = "Join";
-            document.getElementById('joinBtn').style.backgroundColor = "#E67E22";
+          document.getElementById("joinBtn").innerHTML = "Join";
+          document.getElementById("joinBtn").style.backgroundColor = "#E67E22";
         }, 1000);
         setCode("");
-      }      
+      }
     } else {
-      console.log("empty value");      
+      console.log("empty value");
     }
   }
 
