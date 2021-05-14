f713bc613b1d57fb4f12b7fdd423469d12ff5e7d
Parth Chaturvedi <pchaturvedi@my.bcit.ca>
2021-05-14 22:01:08 +0530

    class names changed

--- client/src/interface/AboutUs.js
+++ client/src/interface/AboutUs.js
@@ -11,9 +11,9 @@ export default function AboutUs(setIsAboutUs) {
 
     return (
         <>            
-            <div className="head">
+            <div className="abouthead">
                 
-                <h1><ArrowBackIosIcon
+                <h1 className='abouth1'><ArrowBackIosIcon
                     style={{font: 40}}
                     onClick={() => setIsAboutUs === false}
                 />About Us</h1>
@@ -24,39 +24,39 @@ export default function AboutUs(setIsAboutUs) {
             </div>
             <h2 className='team'>Our Team</h2>
             <div className='row'>
-                <div className='card'>
-                    <img src={Parth} alt='Parth' />
-                    <div className='info'>
-                        <h2>Parth Chaturvedi</h2>
-                        <p className='title'>Coder</p>
+                <div className='aboutcard'>
+                    <img className="aboutimg" src={Parth} alt='Parth' />
+                    <div className='aboutinfo'>
+                        <h2 className="abouth2">Parth Chaturvedi</h2>
+                        <p className='abouttitle'>Coder</p>
                         {/* <a href={`mailto: pchaturvedi@my.bcit.ca`}><button className='btn'>Contact</button></a> */}
                     </div>
                 </div>
 
-                <div className='card'>
-                    <img src={Akshay} alt='Akshay' />
-                    <div className='info'>
-                        <h2>Akshay Marwah</h2>
-                        <p className='title'>Coder</p>
+                <div className='aboutcard'>
+                    <img className="aboutimg" src={Akshay} alt='Akshay' />
+                    <div className='aboutinfo'>
+                        <h2 className="abouth2">Akshay Marwah</h2>
+                        <p className='abouttitle'>Coder</p>
                         {/* <button className='btn'><a href='mailto: etan34@my.bcit.ca'>Contact</a></button> */}
                     </div>
                 </div>
 
 
-                <div className='card'>
-                    <img src={Eric} alt='Eric' />
-                    <div className='info'>
-                        <h2>Eric Tan</h2>
-                        <p className='title'>Scrum Master</p>
+                <div className='aboutcard'>
+                    <img className="aboutimg" src={Eric} alt='Eric' />
+                    <div className='aboutinfo'>
+                        <h2 className="abouth2">Eric Tan</h2>
+                        <p className='abouttitle'>Scrum Master</p>
                         {/* <button className='btn'><a href='mailto: amarwah4@my.bcit.ca'>Contact</a></button> */}
                     </div>
                 </div>
 
-                <div className='card'>
-                    <img src={Boki} alt='Boki' />
-                    <div className='info'>
-                        <h2>Borivoj Pantic</h2>
-                        <p className='title'>Product Manager</p>
+                <div className='aboutcard'>
+                    <img className="aboutimg" src={Boki} alt='Boki' />
+                    <div className='aboutinfo'>
+                        <h2 className="abouth2">Borivoj Pantic</h2>
+                        <p className='abouttitle'>Product Manager</p>
                         {/* <button className='btn'><a href='mailto: bpantic@my.bcit.ca'>Contact</a></button> */}
                     </div>
                 </div>
