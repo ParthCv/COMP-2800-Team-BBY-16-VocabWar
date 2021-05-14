7fc699851a134a17fba2590ce0208f90598a3e0b
akmarwah03 <amarwah4@my.bcit.ca>
2021-05-14 10:58:08 -0700

    Lobby displays name

--- client/src/game/points.js
+++ client/src/game/points.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./points.css";
 import { useFirestoreDocData } from "reactfire";
 
@@ -6,11 +6,20 @@ export default function Points(props) {
   const points = useFirestoreDocData(props.gameRef).data[
     `p${props.player}Points`
   ];
+  const [name, setName] = useState("Player");
+  const gameData = useFirestoreDocData(props.gameRef).data;
+  useEffect(() => {
+    if (props.player === 1) {
+      setName(gameData.p1Name);
+    } else {
+      setName(gameData.p2Name);
+    }
+  }, []);
   const id = `player${props.player}`;
 
   return (
     <div id={id}>
-      <h2>Player {props.player}</h2>
+      <h2>{name}</h2>
       <h2>{points}</h2>
     </div>
   );
