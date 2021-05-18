58b2c1b1eb2082eb6432f8c38442af377358d29d
akmarwah03 <amarwah4@my.bcit.ca>
2021-05-18 09:37:22 -0700

    Worker API timer working

--- client/src/game/Timer.js
+++ client/src/game/Timer.js
@@ -1,30 +1,50 @@
 import React, { useState, useEffect } from "react";
+import * as workerTimers from "worker-timers";
 import "./timer.css";
 
 function Timer({ minutes = 0, seconds = 30, gameRef }) {
   const [over, setOver] = useState(false);
-  const [[m, s], setTime] = useState([minutes, seconds]);
+  const [time, setTime] = useState({ minutes, seconds });
+  const [timerID, setTimerID] = useState(0);
+  useEffect(() => {
+    const interval = workerTimers.setInterval(() => {
+      if (!over) {
+        setTime((prev) => {
+          if (prev.minutes === 0 && prev.seconds === 0) {
+            setOver(true);
+            return { minutes: 0, seconds: 0 };
+          } else if (prev.minutes > 0 && prev.seconds === 0) {
+            return { minutes: prev.minutes - 1, seconds: 59 };
+          } else {
+            return { minutes: prev.minutes, seconds: prev.seconds - 1 };
+          }
+        });
+      }
+    }, 1000);
+    setTimerID(interval);
+  }, []);
 
-  const changeTime = () => {
-    if (over) {
-      return;
-    }
-    if (m === 0 && s === 0) {
-      setOver(true);
-    } else if (s === 0) {
-      setTime([m - 1, 59]);
-    } else {
-      setTime([m, s - 1]);
-    }
-  };
+  // const changeTime = () => {
+  //   if (over) {
+  //     return;
+  //   }
+  //   if (m === 0 && s === 0) {
+  //     setOver(true);
+  //   } else if (s === 0) {
+  //     setTime([m - 1, 59]);
+  //   } else {
+  //     setTime([m, s - 1]);
+  //   }
+  // };
+
+  // useEffect(() => {
+  //   const timerInterval = workerTimers.setInterval(() => changeTime(), 1000);
+  //   return () => workerTimers.clearInterval(timerInterval);
+  // });
 
   useEffect(() => {
-    const timerInterval = setInterval(() => changeTime(), 1000);
-    return () => clearInterval(timerInterval);
-  });
-
-  useEffect(() => {
     if (over) {
+      workerTimers.clearInterval(timerID);
       gameRef.set(
         {
           over: "true",
@@ -39,7 +59,9 @@ function Timer({ minutes = 0, seconds = 30, gameRef }) {
       <h1 className='time'>
         {over
           ? "Times's Up"
-          : `${m.toString().padStart(2, "0")}:${s.toString().padStart(2, "0")}`}
+          : `${time.minutes.toString().padStart(2, "0")}:${time.seconds
+              .toString()
+              .padStart(2, "0")}`}
       </h1>
     </>
   );
