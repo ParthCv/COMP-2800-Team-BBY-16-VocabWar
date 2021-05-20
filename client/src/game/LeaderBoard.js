4b66d5915b2188d53f24f8984215b65e2b1fa3eb
akmarwah03 <amarwah4@my.bcit.ca>
2021-05-20 09:35:23 -0700

    Small adjustments for top user object

--- client/src/game/LeaderBoard.js
+++ client/src/game/LeaderBoard.js
@@ -1,29 +1,40 @@
-import React, {useEffect} from "react";
-import {useFirestoreDocData, useFirestore, useAuth, useFirestoreCollection } from "reactfire";
+import React, { useEffect } from "react";
+import {
+  useFirestoreDocData,
+  useFirestore,
+  useAuth,
+  useFirestoreCollection,
+} from "reactfire";
 import Points from "./points";
 import "./LeaderBoard.css";
 
 export default function LeaderBoard({ props }) {
-
   // const points = useFirestoreDocData(usersRef).data?.points;
   // const auth = useAuth();
   // const users = useFirestore().collection("Users");
   // const userArray = useFirestoreCollection(users).data?.docs;
   // const points = useFirestoreDocData(userArray).data[`points`];
 
-  const users = useFirestore().collection("Users").orderBy("points", "desc").limit(10);
+  const users = useFirestore()
+    .collection("Users")
+    .orderBy("points", "desc")
+    .limit(10);
   const userArray = useFirestoreCollection(users).data?.docs;
 
-  const points = []
-  const name = []
-  userArray.forEach((user) => {
-  points.push(<li key="{points}"> <b>name:</b>{user.data().nickname}  <b>points:</b> {user.data().points}</li>);
-  })
+  const top = [];
+  userArray?.forEach((user) => {
+    top.push({
+      id: user.id,
+      nickname: user.data().nickname,
+      points: user.data().points,
+    });
+  });
+
+  console.log(top);
 
   // userArray.forEach((user) => {
   //   name.push(<li>{user.data().nickname}</li>);
   //   })
-
 
   // useEffect(() => {
   //   document
@@ -33,12 +44,17 @@ export default function LeaderBoard({ props }) {
 
   // });
 
-    return (
-     <div>
-      <h1> Top 10 LeaderBoards  </h1>
-      <div id="listItems">
-        {points}
+  return (
+    <div>
+      <h1> Top 10 LeaderBoards </h1>
+      <div id='listItems'>
+        {top.map((item) => (
+          <h1 key={item.id}>
+            {item.nickname}
+            {item.points}
+          </h1>
+        ))}
       </div>
-     </div>
-    );
-  }
+    </div>
+  );
+}
