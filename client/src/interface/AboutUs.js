59547383b44396003baf3fe27b01308cc49fea3c
akmarwah03 <amarwah4@my.bcit.ca>
2021-05-21 10:44:13 -0700

    Changed some links:

--- client/src/interface/AboutUs.js
+++ client/src/interface/AboutUs.js
@@ -45,7 +45,7 @@ export default function AboutUs({ setIsAboutUs }) {
           <div className='aboutinfo'>
             <h2 className='abouth2'>Akshay Marwah</h2>
             <p className='abouttitle'>Coder</p>
-            <a href='mailto: etan34@my.bcit.ca'>
+            <a href='mailto: amarwah4@my.bcit.ca'>
               <button className='btn'>Contact</button>
             </a>
           </div>
@@ -56,7 +56,7 @@ export default function AboutUs({ setIsAboutUs }) {
           <div className='aboutinfo'>
             <h2 className='abouth2'>Eric Tan</h2>
             <p className='abouttitle'>Scrum Master</p>
-            <a href='mailto: amarwah4@my.bcit.ca'>
+            <a href='mailto: etan34@my.bcit.ca'>
               <button className='btn'>Contact</button>
             </a>
           </div>
